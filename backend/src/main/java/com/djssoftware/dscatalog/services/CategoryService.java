a7ffac4598297518db829881a4ab19990ea81ee9
Divino J. Silva <divino_j_silva@hotmail.com>
2022-05-23 16:18:49 -0300

    Auditing data

--- backend/src/main/java/com/djssoftware/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/djssoftware/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.djssoftware.dscatalog.dto.CategoryDTO;
 import com.djssoftware.dscatalog.entities.Category;
 import com.djssoftware.dscatalog.repositories.CategoryRepository;
+import com.djssoftware.dscatalog.services.exceptions.DatabaseException;
 import com.djssoftware.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -51,6 +54,19 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
+		}
+
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 
 	}
